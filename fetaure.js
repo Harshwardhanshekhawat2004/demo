3ce4cd1986d42c07c05f6bce6bda51dc282cb4a2
harshwardhan <harshwardhanshekhawat2004@gmail.com>
2025-04-29 12:04:24 +0530

    for sure

--- fetaure.js
+++ fetaure.js
@@ -8,3 +8,13 @@ function hello(){
     console.log();
 }
 hello();
+let message = "Hello, this is JavaScript!";
+for (let i = 0; i < 10; i++) {
+    console.log(`${i + 1}: ${message}`);
+}
+
+function greet(name) {
+    return `Welcome, ${name}!`;
+}
+
+console.log(greet("Harsh"));
